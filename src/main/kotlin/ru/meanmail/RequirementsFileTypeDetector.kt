7cb37afbb0b012c2321fc318f673e64bb369b807
meanmail <meanmail@meanmail.ru>
2020-12-05 21:10:49 +0700

    Improve files detection

---
+++ src/main/kotlin/ru/meanmail/RequirementsFileTypeDetector.kt
@@ -0,0 +1,18 @@
+package ru.meanmail
+
+import com.intellij.openapi.fileTypes.FileType
+import com.intellij.openapi.fileTypes.FileTypeRegistry
+import com.intellij.openapi.util.io.ByteSequence
+import com.intellij.openapi.vfs.VirtualFile
+
+class RequirementsFileTypeDetector : FileTypeRegistry.FileTypeDetector {
+    override fun detect(
+        file: VirtualFile, firstBytes: ByteSequence, firstCharsIfText: CharSequence?
+    ): FileType? {
+        if (file.parent.name == "requirements") {
+            return RequirementsFileType
+        }
+
+        return null
+    }
+}
