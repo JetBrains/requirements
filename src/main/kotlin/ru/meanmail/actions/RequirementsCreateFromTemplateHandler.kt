8629ba2d1255c29b37694918b074c177d40b7018
meanmail <meanmail@meanmail.ru>
2021-02-28 00:02:36 +0700

    Fix create new template handler

--- src/main/kotlin/ru/meanmail/actions/RequirementsCreateFromTemplateHandler.kt
+++ src/main/kotlin/ru/meanmail/actions/RequirementsCreateFromTemplateHandler.kt
@@ -2,17 +2,19 @@ package ru.meanmail.actions
 
 import com.intellij.ide.fileTemplates.DefaultCreateFromTemplateHandler
 import com.intellij.ide.fileTemplates.FileTemplate
-import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectManager
 import ru.meanmail.getInstalledPackages
 
 class RequirementsCreateFromTemplateHandler : DefaultCreateFromTemplateHandler() {
     override fun prepareProperties(
         props: MutableMap<String, Any>,
-        filename: String?,
-        template: FileTemplate,
-        project: Project
+        filename: String?, template: FileTemplate
     ) {
-        super.prepareProperties(props, filename, template, project)
+        super.prepareProperties(props, filename, template)
+        val projectName = props.getOrDefault("PROJECT_NAME", null) ?: return
+        val project = ProjectManager.getInstance()
+            .openProjects
+            .find { it.name == projectName } ?: return
 
         props["packages"] = getInstalledPackages(project).map {
             it.name + "==" + it.version
